9781854dcdc1c6ea700c271eced6fa1fd58295e9
akashkumaraguru <akashkumaraguru10@gmail.com>
2025-05-03 13:15:19 +0530

    Login-completed

--- src/Login.tsx
+++ src/Login.tsx
@@ -4,25 +4,48 @@ import Img from "./assets/Side-image.png";
 function Login() {
 
     return (
-        <div className="lg:max-w-2xl">
-          <div className="flex flex-col">
 
-          <div className="">
-                <img src={Img} alt="SideBar" className="w-[680px] object-fit lg:max-h-screen  absolute"/>
-                <h1 className="relative text-white lg:font-semibold text-3xl px-[40%] py-[50%] ">Walkin</h1>
+        <div className="grid grid-cols-2 w-screen h-screen flex ">
+            <div className="">
+                <img src={Img} alt="Side-Img" className="absolute lg:w-1/2 lg:h-full object-cover rounded-r-3xl" />
+                <h1 className="relative mt-100 ml-80 text-3xl font-semibold text-white font-[poppins]">Walkin</h1>
             </div>
 
-            <div className="bg-black w-full z-30">
+
+            <div className="mt-50">
+                <h1 className="font-semibold ml-51 text-blue-500 text-[24px]">Login</h1>
+
+                <div className="flex flex-col items-center">
+                    <input
+                        type="text"
+                        name=""
+                        id="mailinput"
+                        placeholder="Email or phone number"
+                        className="bg-[#F2F2F2] stroke-[#E5E5E5] p-4 w-[360px] h-[48px] mt-10 focus:outline-none rounded-[6px]"
+                    />
+                    <input
+                        type="text"
+                        name=""
+                        id="password"
+                        placeholder="Enter your password"
+                        className="bg-[#F2F2F2] stroke-[#E5E5E5] p-4 w-[360px] h-[48px] mt-4 focus:outline-none rounded-[6px]"
+                    />
+
+                    <a href="#" className="self-start ml-[calc(50%-180px)] mt-4 text-blue-500 text-sm mb-8">Forgot password?</a>
+
+                    <button className="bg-[#007AFF] text-white text-center cursor-pointer  w-[360px] h-[48px] mb-8  focus:outline-none rounded-[6px]">Sign in</button>
+
+                    <div className="w-[360px] h-px bg-gray-300 mt-4 mb-6"></div>
+                    <p>Don't have an Account? <a href="#" className="text-[#007AFF] underline">Sign up Now</a></p>
+
+                </div>
 
             </div>
-            
-          </div>
-
-
-
-
 
         </div>
+
+
+
     )
 }
 
